b0ba4d670745db70d3bcf09c0ddccaef6e35c402
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-20 17:46:17 -0800

    On Step 6, completed steps a-c.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 /* global data */
 /* exported data */
+var nextEntryID = 1;
 function imageGenerator(event) {
+  event.preventDefault();
   $setImageURL.setAttribute('src', event.target.value);
 }
+
+function retrieveEntryInfo(event) {
+  var entryInfo = {};
+  event.preventDefault();
+  var titleValue = $getInfoFromSubmission.elements.title.value;
+  var urlValue = $getInfoFromSubmission.elements.url.value;
+  var messageValue = $getInfoFromSubmission.elements.message.value;
+  entryInfo.title = titleValue;
+  entryInfo.url = urlValue;
+  entryInfo.message = messageValue;
+  entryInfo.nextEntryID = nextEntryID++;
+}
 var $setImageURL = document.querySelector('#url-value');
-var $entryInputDetection = document.querySelector('#url');
+var $entryInputDetection = document.querySelector('#photoURL');
 $entryInputDetection.addEventListener('blur', imageGenerator);
+
+var $getInfoFromSubmission = document.querySelector('#get-entry-form');
+$getInfoFromSubmission.addEventListener('submit', retrieveEntryInfo);
