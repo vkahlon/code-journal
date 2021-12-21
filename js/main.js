cfb9d81ea78a1f7437d3da7e04ba5a312303e619
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-20 16:52:28 -0800

    Completed Step 5: listen for input events on the photourl and update the src step with preview.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 /* global data */
 /* exported data */
+function imageGenerator(event) {
+  $setImageURL.setAttribute('src', event.target.value);
+}
+var $setImageURL = document.querySelector('#url-value');
+var $entryInputDetection = document.querySelector('#url');
+$entryInputDetection.addEventListener('blur', imageGenerator);
