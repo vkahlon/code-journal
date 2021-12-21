053e6a4f2b151fd07bbc3d7819fe548088cd5230
Vickram Kahlon <vickram.kahlon@gmail.com>
2021-12-20 20:32:55 -0800

    Completed Step 7- local storage is active.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+window.addEventListener('beforeunload', retrieveData);
+var previousEntriesJSON = localStorage.getItem('total-entries');
+if (previousEntriesJSON != null) {
+  data.entries = JSON.parse(previousEntriesJSON);
+}
+function retrieveData(event) {
+  var entriesJSON = JSON.stringify(data.entries);
+  localStorage.setItem('total-entries', entriesJSON);
+}
